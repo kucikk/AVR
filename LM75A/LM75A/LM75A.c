eee94a8bdec93a218fa0a91306f7fd0335497c40
Tomáš Kučera <me@kucikk.cz>
2015-01-04 13:48:01 +0100

    přepracovaný zobrazování naměřené teploty

--- LM75A/LM75A/LM75A.c
+++ LM75A/LM75A/LM75A.c
@@ -88,7 +88,7 @@ uint8_t TWIGetStatus (void) {
 /************************************************************************/
 /* Pokusí se přečíst teplotu z čidla                                    */
 /************************************************************************/
-uint8_t getTemp (uint8_t *u8data) {
+uint8_t getTemp (uint8_t *temp_hi, uint8_t *temp_lo) {
 	
 	TWIStart();// S - Start
 	if (TWIGetStatus() != TW_START)// je I2C nastartováno?
@@ -110,11 +110,11 @@ uint8_t getTemp (uint8_t *u8data) {
 	if (TWIGetStatus() != TW_MR_SLA_ACK)
 		return ERROR;
 	
-	*u8data = TWIReadACK();// načte MSB (důležitější bajt) - celá část teploty = poměrně zajímavé číslo
+	*temp_hi = TWIReadACK();// načte MSB (důležitější bajt) - celá část teploty = poměrně zajímavé číslo
 	if (TWIGetStatus() != TW_MR_DATA_ACK)
 		return ERROR;
 	
-	TWIReadNACK();//načte LSB (méně důležitý bajt) - desetinná část teploty = nezajímá nás
+	*temp_lo = TWIReadNACK();//načte LSB (méně důležitý bajt) - desetinná část teploty = nezajímá nás
 	if (TWIGetStatus() != TW_MR_DATA_NACK)
 		return ERROR;
 	
@@ -123,53 +123,74 @@ uint8_t getTemp (uint8_t *u8data) {
 	return SUCCESS;
 }
 
+
+void LEDInit (void) {
+	DDRB = 0x0e;
+	PORTB = 0x00;
+}
+
+
+void LEDTest (void) {
+	
+	PORTB |= 1 << PB1;
+	_delay_ms(500);
+	PORTB &= ~(1 << PB1);
+	
+	PORTB |= 1 << PB2;
+	_delay_ms(500);
+	PORTB &= ~(1 << PB2);
+	
+	PORTB |= 1 << PB3;
+	_delay_ms(500);
+	PORTB &= ~(1 << PB3);
+}
+
 int main(void) {
 	
-	uint8_t temp = 0;
-	//uint8_t t = 0;
+	uint8_t temp_hi = 0;
+	uint8_t temp_lo = 0;
 	
-	DDRB = 0xff;
-	PORTB = 0xff;
-	
-	DDRD = 0x04;
-	PORTD = 0x00;
-	
-	_delay_ms(1000);
-	PORTB = 0x00;
-	_delay_ms(1000);
+	LEDInit();
+	LEDTest();
 	
 	TWIInit();
 	
 	while(1) {
-		if (getTemp(&temp) == ERROR)
+		PORTB &= ~(1 << PB1);
+		
+		if (getTemp(&temp_hi, &temp_lo) == ERROR) {
+			PORTB |= (1 << PB1);
+			TWIStop();
 			continue;
-		PORTB = temp;
-		/*
-		if ((temp & 0x80) == 1) {// záporné číslo
-			PORTB = 0x10;// poslední ledka
-		} else {
-			t = temp & 0x7f;
-			
-			if (t > 30) {
-				PORTB = 0x10;//poslední ledka
-			} else if (t > 29) {
-				PORTB = 0x1f;//všechny ledky
-			} else if (t > 28) {
-				PORTB = 0x0f;//krom poslední ledky
-			} else if (t > 27) {
-				PORTB = 0x07;//první 3 ledky
-			} else if (t > 26) {
-				PORTB = 0x03;//první 2 ledky
-			} else if (t > 25) {
-				PORTB = 0x01;//první ledka
-			} else if (t > 24) {
-				PORTB = 0x02;//předposlední ledka
-			} else if (t > 23) {
-				PORTB = 0x04;//prostřední ledka
-			} else {
-				PORTB = 0x08;//předposlední ledka
-			}
 		}
-		*/
+		
+		PORTB &= ~(1 << PB3);
+		
+		if (temp_hi >= 20) {
+			temp_hi -= 20;
+			PORTB |= (1 << PB2);
+		} else {
+			PORTB &= ~(1 << PB2);
+		}
+		
+		for (uint8_t i = 0; i < temp_hi; i++) {
+			PORTB |= (1 << PB3);
+			_delay_ms(250);
+			PORTB &= ~(1 << PB3);
+			_delay_ms(250);
+		}
+		
+		_delay_ms(1000);
+		
+		PORTB &= ~(1 << PB2);
+		
+		for (uint8_t i = 0; i < temp_lo >> 5; i++) {
+			PORTB |= (1 << PB3);
+			_delay_ms(250);
+			PORTB &= ~(1 << PB3);
+			_delay_ms(250);
+		}
+	
+		_delay_ms(1000);
     }
 }
